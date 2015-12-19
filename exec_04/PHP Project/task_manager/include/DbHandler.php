2ca8842af89b533c77831894ee6585b4cd6b6359
oliveirawro <oliveira@woliveira.net>
2015-12-19 18:01:42 -0200

    Adicionado exercicio 04

---
+++ exec_04/PHP Project/task_manager/include/DbHandler.php
@@ -0,0 +1,249 @@
+<?php
+
+class DbHandler {
+
+    private $conn;
+
+    function __construct() {
+        require_once dirname(__FILE__) . '/DbConnect.php';
+        $db = new DbConnect();
+        $this->conn = $db->connect();
+    }
+
+    /* cria novo usuário    */
+    public function createUser($name, $email, $password) {
+        require_once 'PassHash.php';
+        $response = array();
+
+        // Usuário existente
+        if (!$this->isUserExists($email)) {
+            // hash senha
+            $password_hash = PassHash::hash($password);
+
+            // gera api
+            $api_key = $this->generateApiKey();
+
+            $stmt = $this->conn->prepare("INSERT INTO users(name, email, password_hash, api_key, status) values(?, ?, ?, ?, 1)");
+            $stmt->bind_param("ssss", $name, $email, $password_hash, $api_key);
+
+            $result = $stmt->execute();
+
+            $stmt->close();
+
+            if ($result) {
+                return USER_CREATED_SUCCESSFULLY;
+            } else {
+                return USER_CREATE_FAILED;
+            }
+        } else {
+            return USER_ALREADY_EXISTED;
+        }
+
+        return $response;
+    }
+
+    /* checa login */
+    public function checkLogin($email, $password) {
+
+        $stmt = $this->conn->prepare("SELECT password_hash FROM users WHERE email = ?");
+
+        $stmt->bind_param("s", $email);
+
+        $stmt->execute();
+
+        $stmt->bind_result($password_hash);
+
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+
+            $stmt->fetch();
+
+            $stmt->close();
+
+            if (PassHash::check_password($password_hash, $password)) {
+                return TRUE;
+            } else {
+                return FALSE;
+            }
+        } else {
+            $stmt->close();
+
+            return FALSE;
+        }
+    }
+
+    /** checa duplicado     */
+    private function isUserExists($email) {
+        $stmt = $this->conn->prepare("SELECT id from users WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->store_result();
+        $num_rows = $stmt->num_rows;
+        $stmt->close();
+        return $num_rows > 0;
+    }
+
+    /* pega usuário por email  */
+    public function getUserByEmail($email) {
+        $stmt = $this->conn->prepare("SELECT name, email, api_key, status, created_at FROM users WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        if ($stmt->execute()) {
+            // $user = $stmt->get_result()->fetch_assoc();
+            $stmt->bind_result($name, $email, $api_key, $status, $created_at);
+            $stmt->fetch();
+            $user = array();
+            $user["name"] = $name;
+            $user["email"] = $email;
+            $user["api_key"] = $api_key;
+            $user["status"] = $status;
+            $user["created_at"] = $created_at;
+            $stmt->close();
+            return $user;
+        } else {
+            return NULL;
+        }
+    }
+
+    /* usuario api    */
+    public function getApiKeyById($user_id) {
+        $stmt = $this->conn->prepare("SELECT api_key FROM users WHERE id = ?");
+        $stmt->bind_param("i", $user_id);
+        if ($stmt->execute()) {
+            // $api_key = $stmt->get_result()->fetch_assoc();
+            //terminar
+            $stmt->bind_result($api_key);
+            $stmt->close();
+            return $api_key;
+        } else {
+            return NULL;
+        }
+    }
+
+    public function getUserId($api_key) {
+        $stmt = $this->conn->prepare("SELECT id FROM users WHERE api_key = ?");
+        $stmt->bind_param("s", $api_key);
+        if ($stmt->execute()) {
+            $stmt->bind_result($user_id);
+            $stmt->fetch();
+            // terminar
+            // $user_id = $stmt->get_result()->fetch_assoc();
+            $stmt->close();
+            return $user_id;
+        } else {
+            return NULL;
+        }
+    }
+
+    /**
+     * Validating user api key
+     * If the api key is there in db, it is a valid key
+     * @param String $api_key user api key
+     * @return boolean
+     */
+    public function isValidApiKey($api_key) {
+        $stmt = $this->conn->prepare("SELECT id from users WHERE api_key = ?");
+        $stmt->bind_param("s", $api_key);
+        $stmt->execute();
+        $stmt->store_result();
+        $num_rows = $stmt->num_rows;
+        $stmt->close();
+        return $num_rows > 0;
+    }
+
+    /**
+     * Generating random Unique MD5 String for user Api key
+     */
+    private function generateApiKey() {
+        return md5(uniqid(rand(), true));
+    }
+
+    /* nova tarefa  */
+    public function createTask($user_id, $task) {
+        $stmt = $this->conn->prepare("INSERT INTO tasks(task) VALUES(?)");
+        $stmt->bind_param("s", $task);
+        $result = $stmt->execute();
+        $stmt->close();
+
+        if ($result) {
+            // task row created
+            // now assign the task to user
+            $new_task_id = $this->conn->insert_id;
+            $res = $this->createUserTask($user_id, $new_task_id);
+            if ($res) {
+                return $new_task_id;
+            } else {
+                return NULL;
+            }
+        } else {
+            return NULL;
+        }
+    }
+
+    /** única tarefa  */
+    public function getTask($task_id, $user_id) {
+        $stmt = $this->conn->prepare("SELECT t.id, t.task, t.status, t.created_at from tasks t, user_tasks ut WHERE t.id = ? AND ut.task_id = t.id AND ut.user_id = ?");
+        $stmt->bind_param("ii", $task_id, $user_id);
+        if ($stmt->execute()) {
+            $res = array();
+            $stmt->bind_result($id, $task, $status, $created_at);
+            // TODO
+            // $task = $stmt->get_result()->fetch_assoc();
+            $stmt->fetch();
+            $res["id"] = $id;
+            $res["task"] = $task;
+            $res["status"] = $status;
+            $res["created_at"] = $created_at;
+            $stmt->close();
+            return $res;
+        } else {
+            return NULL;
+        }
+    }
+
+    /** todas as tarefas do usuario   */
+    public function getAllUserTasks($user_id) {
+        $stmt = $this->conn->prepare("SELECT t.* FROM tasks t, user_tasks ut WHERE t.id = ut.task_id AND ut.user_id = ?");
+        $stmt->bind_param("i", $user_id);
+        $stmt->execute();
+        $tasks = $stmt->get_result();
+        $stmt->close();
+        return $tasks;
+    }
+
+    /** atualiza tarefa  */
+    public function updateTask($user_id, $task_id, $task, $status) {
+        $stmt = $this->conn->prepare("UPDATE tasks t, user_tasks ut set t.task = ?, t.status = ? WHERE t.id = ? AND t.id = ut.task_id AND ut.user_id = ?");
+        $stmt->bind_param("siii", $task, $status, $task_id, $user_id);
+        $stmt->execute();
+        $num_affected_rows = $stmt->affected_rows;
+        $stmt->close();
+        return $num_affected_rows > 0;
+    }
+
+    /* remove tarefa */
+    public function deleteTask($user_id, $task_id) {
+        $stmt = $this->conn->prepare("DELETE t FROM tasks t, user_tasks ut WHERE t.id = ? AND ut.task_id = t.id AND ut.user_id = ?");
+        $stmt->bind_param("ii", $task_id, $user_id);
+        $stmt->execute();
+        $num_affected_rows = $stmt->affected_rows;
+        $stmt->close();
+        return $num_affected_rows > 0;
+    }
+
+    /* atribui tarefa ao usuario    */
+    public function createUserTask($user_id, $task_id) {
+        $stmt = $this->conn->prepare("INSERT INTO user_tasks(user_id, task_id) values(?, ?)");
+        $stmt->bind_param("ii", $user_id, $task_id);
+        $result = $stmt->execute();
+
+        if (false === $result) {
+            die('execute() falhou: ' . htmlspecialchars($stmt->error));
+        }
+        $stmt->close();
+        return $result;
+    }
+
+}
+
+?>
